04bfcf9658dede5562f0d81a76745acd3dcb12b0
Daniela Otero <47904871+dani97daniela@users.noreply.github.com>
2024-02-27 21:46:24 -0500

    Updating passwordGenerator to start including tkinter GUI

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,5 +1,24 @@
 #PassWord Generator
 import random
+#classic tkinter
+import tkinter as tk
+#themed tkinter
+import tkinter.ttk as tkk
+
+window = tk.Tk()
+
+intro = tk.Label(text = "PassWord Generator",fg = "white",bg = "black")
+#This is important if you ever want to minimize the GUI the intro text won't be affected
+intro.pack()
+
+entry = tk.Entry(fg = "white", bg= "black")
+entry.pack()
+characterAmount = entry.get()
+
+generatePasswordButton = tk.Button(text = "Generate Password",width = 10,height = 5,fg = "black",bg = "white")
+generatePasswordButton.pack()
+
+window.mainloop()
 
 print("Welcome to your Password Generator!")
 
